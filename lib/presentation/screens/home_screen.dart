c85947626762ea5d714e80329cd3c4575bc342e1
AleXx815 <davgoga815@gmail.com>
2023-07-19 14:30:19 -0500

    solicitar permisos

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,11 +9,13 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Estado de los permisos"),
+        title: context.select(
+          (NotificationsBloc bloc) => Text("${bloc.state.status}"),
+        ),
         actions: [
           IconButton(
             onPressed: () {
-              // TODO PEDIR PERMISO PARA LAS NOTIFICACIONES
+              context.read()<NotificationsBloc>().requestPermission();
             },
             icon: const Icon(Icons.settings),
           ),
